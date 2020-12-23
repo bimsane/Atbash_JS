f1970b47b1b3bd190ea1d38e0996106292d3dcfb
Agustin Torres <BIMsane@outlook.com>
2020-12-23 17:05:17 -0500

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 function Decypher(data) {
   for (let i = 0; i < data.length; i++) {
-    const element = String(data[i]).toLowerCase().trim();
+    const element = String(data[i]).toLowerCase();
     var index = alphabet.indexOf(element);
     if(isCharacterALetter(element)){
       console.log(String(element).toLowerCase() + " : " + alphabet[25 - index]);
