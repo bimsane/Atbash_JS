efeafb83444fc5e9d5c4f8b6bb80a0e854fd90ac
Agustin Torres <BIMsane@outlook.com>
2020-12-23 17:16:26 -0500

    another commit

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-function Decypher(data) {
+function Convert(data) {
   let output = "";
   for (let i = 0; i < data.length; i++) {
     const element = String(data[i]).toLowerCase();
     var index = alphabet.indexOf(element);
-    if(isCharacterALetter(element)){
+    if (isCharacterALetter(element)) {
       output += alphabet[25 - index];
-    }
-    else{
+    } else {
       output += element;
     }
   }
   console.log(output);
 }
 function isCharacterALetter(char) {
-  return (/[a-zA-Z]/).test(char)
+  return /[a-zA-Z]/.test(char);
 }
 
-Decypher('Dvoxlnv gl gsv qfmtov');
+Convert("Dvoxlnv gl gsv qfmtov");
+Convert("Have A Nice Day!");
