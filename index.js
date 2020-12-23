6a877a325d5f96674a6f786c1ce477c2ed5fd162
Agustin Torres <BIMsane@outlook.com>
2020-12-23 17:13:37 -0500

    cleaned up some

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 function Decypher(data) {
+  let output = "";
   for (let i = 0; i < data.length; i++) {
     const element = String(data[i]).toLowerCase();
     var index = alphabet.indexOf(element);
     if(isCharacterALetter(element)){
-      console.log(String(element).toLowerCase() + " : " + alphabet[25 - index]);
+      output += alphabet[25 - index];
     }
   }
+  console.log(output);
 }
 function isCharacterALetter(char) {
   return (/[a-zA-Z]/).test(char)
